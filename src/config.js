1a1168436a4a5925fb0e9fd49e107c8a4b0436cd
KarolinaGroszewska <40745949+KarolinaGroszewska@users.noreply.github.com>
2020-10-03 02:36:36 -0500

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyBbFKzERYVK8RxM6dn50AjbvIuAysWAeHI",
+    authDomain: "https://covidtestspeedrun.web.app/",
+    databaseURL: "https://console.firebase.google.com/u/0/project/covidspeedrun/database/covidspeedrun/data",
+  }
+  
+  export default config;
