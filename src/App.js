41556270eb8c4eb7bebe78ee6d6879f2a0facfd1
KarolinaGroszewska <40745949+KarolinaGroszewska@users.noreply.github.com>
2020-10-03 02:36:06 -0500

    firebase import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './App.scss'
 import { Router, Route, Switch } from 'react-router-dom'
 import history from './history'
-
+import Firebase from 'firebase'
 import Main from './components/Main'
 
 class App extends Component {
