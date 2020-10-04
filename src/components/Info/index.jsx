d1aa1bbaf434aae55cc980074a59b190b11a8a22
Sam Sliman <samsliman@Sams-MacBook-Pro.local>
2020-10-03 23:36:17 -0500

    style info page

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -29,11 +29,55 @@ const Info = ({ setShowInput, setShowInfo }) => {
       <Container id='mainContainer'>
         <Row>
           <Col className={styles.colCenter}>
-            Welcome to the Covid Test Speedrunning Leaderboard!
+            <div className={styles.mainTitle}>
+              Welcome to the Covid Test Speedrunning Leaderboard!
+            </div>
           </Col>
         </Row>
         <Row>
-          <Col className={styles.colCenter}>The rules are simple:</Col>
+          <Col className={styles.colCenter}>
+            <div className={styles.secondaryTitle}>The rules are simple:</div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className={styles.colCenter}>
+            <div className={styles.ternaryTitle}>
+              &#8226;There are two categories, with the details of each listed
+              below.
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className={styles.colCenter}>
+            <div className={styles.ternaryTitle}>
+              &#8226;Time yourself according to the rules of one of the
+              categories, then enter your name and time on the next page.
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className={styles.colCenter}>
+            <div className={styles.ternaryTitle}>
+              &#8226;Finally, you have the option to upload a video to verify
+              your time and be placed on a special "Verified" leaderboard.
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className={styles.colCenter}>
+            <div className={styles.ternaryTitle}>
+              &#8226;While doing this, you must do your test properly, follow
+              the rules of the rec center, and respect all rec center employees.
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className={styles.colCenter}>
+            <div className={styles.ternaryTitle}>
+              &#8226;Disrepectful behavior or an inconclusive test result will
+              disqualify your time.
+            </div>
+          </Col>
         </Row>
         <Row>
           <Col className={styles.colCenter}>
@@ -43,11 +87,21 @@ const Info = ({ setShowInput, setShowInfo }) => {
               }
               onClick={() => handleAnyClick()}
             >
-              <div>Any-Per-Spit</div>
-              <div>
-                Time from the moment you open the test kit, until the moment the
-                cap is sealed on.
-              </div>
+              <table>
+                <tr>
+                  <td>
+                    <div className={styles.ternaryTitle}>Any-Per-Spit</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className={styles.smallText}>
+                      Time from the moment you open the test kit, until the
+                      moment the cap is sealed on.
+                    </div>
+                  </td>
+                </tr>
+              </table>
             </button>
             <button
               className={
@@ -55,11 +109,21 @@ const Info = ({ setShowInput, setShowInfo }) => {
               }
               onClick={() => handleEndClick()}
             >
-              <div>End-To-End</div>
-              <div>
-                Time from the moment you enter the rec center, to the moment you
-                exit the back door.
-              </div>
+              <table>
+                <tr>
+                  <td>
+                    <div className={styles.ternaryTitle}>End-To-End</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className={styles.smallText}>
+                      Time from the moment you enter the rec center, to the
+                      moment you exit the back door.
+                    </div>
+                  </td>
+                </tr>
+              </table>
             </button>
           </Col>
         </Row>
